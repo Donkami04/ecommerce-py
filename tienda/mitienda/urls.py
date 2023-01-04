5ee763fec07865ce1a2ce1efe29e071b9b6e674e
Donkami04 <camilo.munerac@gmail.com>
2023-01-04 14:48:02 -0500

    Before add update end point

--- tienda/mitienda/urls.py
+++ tienda/mitienda/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('categories/', views.categories, name='categories'),
     path('categories/<int:category_id>/', views.category_products, name='category_products'),
     path('products/new/', views.create_product, name='create_product'),
-    
+    path('products/modify/', views.modify_product, name='modify_product'),
+    path('products/delete/<int:product_id>/', views.delete_product_confirmation, name='delete_product_confirmation'),
+    path('products/delete/confirmation/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
--- tienda/mitienda/views.py
+++ tienda/mitienda/views.py
@@ -1,7 +1,8 @@
 from mitienda.models import Category, Product
 from mitienda.forms import ProductForm
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
 def categories(request):
     categories = Category.objects.all()
@@ -13,12 +14,25 @@ def category_products(request, category_id):
     return render(request,'mitienda/category_products.html', {'category': category, 'products': products})
 
 def create_product(request):
-    
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponse('Created')
+            return HttpResponseRedirect(reverse('mitienda:category_products', args=request.POST['category']))
+
     else:
         form = ProductForm()
     return render(request, 'mitienda/create_product.html', {'form': form})
+
+def delete_product_confirmation(request, product_id):
+    object = Product.objects.get(id=product_id)
+    return render(request, 'mitienda/delete_product_confirmation.html', {'product': object})
+
+def delete_product(request, product_id):
+    object = Product.objects.get(id=product_id)
+    object_category = object.category.id
+    print('XXXXXXXXXXXXXXXXXXXX ================>',object_category)
+    object.delete()
+    return render(request, 'mitienda/removed.html', {'product': object})
+
+def modify_product(request, product_id):
+    pass
